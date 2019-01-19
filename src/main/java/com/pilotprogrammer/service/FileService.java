4e33bd743c65f48244409d34e9ddb6c00269008f
PilotProgrammer <pilot5698@yahoo.com>
2019-01-18 21:12:58 -0600

    imports

--- src/main/java/com/pilotprogrammer/service/FileService.java
+++ src/main/java/com/pilotprogrammer/service/FileService.java
@@ -1,10 +1,8 @@
 package com.pilotprogrammer.service;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URL;
 
 import javax.servlet.http.HttpServletResponse;
@@ -13,14 +11,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 
 @Path("/file")
 public class FileService {	
